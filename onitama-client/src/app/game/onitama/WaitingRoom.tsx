5ec6414f3b248cc09244b156e23ab91c98a2bf2d
josh kennedy <josh@outthinkgroup.com>
2025-02-28 09:36:28 -0500

    adds player id to waiting room

--- onitama-client/src/app/game/onitama/WaitingRoom.tsx
+++ onitama-client/src/app/game/onitama/WaitingRoom.tsx
@@ -5,6 +5,7 @@ export function WaitingRoom({ playerId }: { playerId: number }) {
 	const URL = typeof window != "undefined" && window.location.href;
   return (
     <div className="waiting-room">
+			<p>You are player {playerId}</p>
       <h1>Waiting For the other player</h1>
       <p>Send them the url if you haven{'\''}t </p>
       <CopyText value={URL || ""} />
