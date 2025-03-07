52cb6cd439aa91152dbeae8cebac18989a196542
josh kennedy <josh@outthinkgroup.com>
2025-03-06 21:16:54 -0500

    yo the double render useEffect is stupid in dev

--- onitama-client/src/components/onitama/ws.tsx
+++ onitama-client/src/components/onitama/ws.tsx
@@ -1,54 +1,38 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
-let ws: WebSocket|undefined;
+let ws: WebSocket | undefined;
 
 export type WSConfigSettings = {
-	gameKind: "singleplayer" | "mulitplayer";
+  gameKind: "singleplayer" | "mulitplayer";
 };
 
 export function useWS({
-	messageHandler,
-	info,
+  messageHandler,
+  info,
 }: {
-	messageHandler: (msg: any) => void;
-	info: WSConfigSettings;
+  messageHandler: (msg: any) => void;
+  info: WSConfigSettings;
 }) {
-	const [socket, setSocket] = useState<WebSocket | undefined>();
-	const gameId =
-		typeof window != "undefined" &&
-		new URLSearchParams(window.location.search).get("gameId");
-	useEffect(() => {
-		console.log("we running")
-		if (!gameId) {
-			console.log("No game Id Found will be given a new one");
-		}
-		if (!ws) {
-			console.log(process.env.NEXT_PUBLIC_BACKEND_URL);
-			const host = process.env.NEXT_PUBLIC_BACKEND_URL;
-			ws = new WebSocket(host + `/ws?gameId=${gameId}&kind=${info.gameKind}`);
-			setSocket(ws);
-		}
-		if (ws) {
-			console.log(ws);
-			ws.onmessage = messageHandler;
-			ws.onclose = function() {
-				console.log("CLOSED CONNECTION");
-				ws = undefined;
-				setSocket(undefined);
-			};
-			ws.onopen = function() {
-				console.log("OPENED CONNECTION");
-			};
-		}
-		return () => {
-			setTimeout(() => {
-				if (ws && ws?.readyState === WebSocket.OPEN) {
-					ws.close(1000, "Client closing connection");
-					setSocket(undefined);
-				}
-			}, 100); // Small delay
-		};
-	}, [messageHandler, info.gameKind, setSocket]);
+  useEffect(() => {
+    if (!ws) {
+      console.log(process.env.NEXT_PUBLIC_BACKEND_URL);
+      const gameId = new URLSearchParams(window.location.search).get("gameId");
+      const host = process.env.NEXT_PUBLIC_BACKEND_URL;
+      ws = new WebSocket(host + `/ws?gameId=${gameId}&kind=${info.gameKind}`);
+    }
+    if (ws) {
+      ws.onmessage = messageHandler;
+      ws.onclose = function () {
+        ws = undefined;
+      };
+      ws.onopen = function () {
+        console.log("OPENED CONNECTION");
+      };
+    }
+    return () => {
+      ws?.close(1000, "Client closing connection");
+    };
+  }, [messageHandler, info.gameKind]);
 
-	return socket;
+  return ws;
 }
