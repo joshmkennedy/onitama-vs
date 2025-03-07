d7e7dac345f102699c837bfd36540c72cc1692ad
josh kennedy <josh@outthinkgroup.com>
2025-03-06 19:22:12 -0500

    correct colors erywhere

--- onitama-client/src/components/onitama/WaitingRoom/WaitingRoom.tsx
+++ onitama-client/src/components/onitama/WaitingRoom/WaitingRoom.tsx
@@ -1,11 +1,16 @@
-"use client"
 import { CopyText } from "../CopyText/CopyText";
+import Menu from "../Menu/Menu";
 import styles from "./WaitingRoom.module.css";
 
-export function WaitingRoom({ playerId }: { playerId: number }) {
+export function WaitingRoom({ playerId, showHelp, newGame }: {
+	playerId: number;
+	showHelp: () => void;
+	newGame: () => void;
+}) {
 	const URL = typeof window != "undefined" && window.location.href;
   return (
     <div className={styles.waitingRoom}>
+			<Menu newGame={newGame} showHelp={showHelp} />
 			<p>You are player {playerId}</p>
       <h1>Waiting For the other player</h1>
       <p>Send them the url if you haven{'\''}t </p>
